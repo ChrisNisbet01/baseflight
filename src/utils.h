1859982221b96be3a57d2a2c44f8b0bd38fd2180
Chris Nisbet <nischris@gmail.com>
2014-08-09 08:18:37 +1200

    CN - Fix divide with rounding macro

--- src/utils.h
+++ src/utils.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define DIVIDE_WITH_ROUNDING( n, d ) (n) < 0 ? ((n)-((d)/2))/(d) : ((n)+((d)/2))/(d)
+#define DIVIDE_WITH_ROUNDING( n, d ) ((n) < 0 ? ((n)-((d)/2))/(d) : ((n)+((d)/2))/(d))
 
 int constrain(int amt, int low, int high);
 // sensor orientation
